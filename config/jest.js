a5afbf785b58f51c15128541cef6b9cc66d77b90
Phillip Gates-Idem <phil.gates-idem@lifeomic.com>
2018-08-15 22:23:47 -0400

    Upgrade dependencies and tweak jest config and tslint config

--- config/jest.js
+++ config/jest.js
@@ -2,6 +2,9 @@ module.exports = {
   transform: {
     '^.+\\.tsx?$': 'ts-jest'
   },
-  testMatch: ['**/*.test.{js,jsx,ts,tsx}'],
+  testMatch: [
+    '<rootDir>/src/**/*.test.{js,jsx,ts,tsx}',
+    '<rootDir>/test/**/*.test.{js,jsx,ts,tsx}'
+  ],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node', 'mjs']
 };
