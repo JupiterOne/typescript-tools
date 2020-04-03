6df7245b53d8f9cef276f0453c726ade143f063c
David Tanner <david.tanner@lifeomic.com>
2020-04-03 15:44:42 -0600

    The order tells jest how to look for files to resolve

--- config/jest.js
+++ config/jest.js
@@ -8,5 +8,5 @@ module.exports = {
     '<rootDir>/test/**/*.test.{js,jsx,ts,tsx}',
     '<rootDir>/lambda/**/*.test.{js,jsx,ts,tsx}',
   ],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node', 'mjs'],
+  moduleFileExtensions: ['js', 'json', 'ts', 'tsx', 'jsx', 'node', 'mjs'],
 };
