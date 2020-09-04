2b822636cc2678a3836669d1b335835544cf1ba9
Phil Gates-Idem <phil.gates-idem@jupiterone.com>
2020-09-04 16:32:56 -0400

    Turn on code coverage at 100% threshold by default (encourages high coverage)

--- config/jest.js
+++ config/jest.js
@@ -4,7 +4,16 @@ module.exports = {
     '<rootDir>/src/**/*.test.{js,jsx,ts,tsx}',
     '<rootDir>/test/**/*.test.{js,jsx,ts,tsx}',
   ],
+  collectCoverage: true,
   collectCoverageFrom: ['src/**/*.ts', '!**/*.test.ts', '!**/__tests__/**'],
+  coverageThreshold: {
+    global: {
+      statements: 100,
+      branches: 100,
+      functions: 100,
+      lines: 100,
+    },
+  },
   testEnvironment: 'node',
   moduleNameMapper: {
     '^~(/.*)$': '<rootDir>$1',
