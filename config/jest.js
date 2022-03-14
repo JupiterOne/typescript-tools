29821c8488fbf59e90193a7a54f74e7cc01caf2e
Phil Gates-Idem <phil.gates-idem@jupiterone.com>
2022-03-14 13:18:07 -0400

    Change jest test coverage reporting to:  "json-summary", "text", "text-summary"

--- config/jest.js
+++ config/jest.js
@@ -1,6 +1,7 @@
 module.exports = {
   ...require('./jest-base'),
   testMatch: ['<rootDir>/{src,test}/**/*.test.{ts,tsx}'],
+  coverageReporters: ['json-summary', 'text', 'text-summary'],
   collectCoverageFrom: [
     'src/**/*.{ts,tsx}',
     '!**/*.test.{ts,tsx}',
