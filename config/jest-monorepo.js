49c9954e6bfb74e42ee5048e8961d01bbf01f072
Phil Gates-Idem <phil.gates-idem@jupiterone.com>
2022-12-28 11:47:02 -0500

    Node.js 18 upgrade Minor adjustments to README and jest config. Use Node.js 18 for building this project

--- config/jest-monorepo.js
+++ config/jest-monorepo.js
@@ -4,4 +4,8 @@ module.exports = {
   ...require('./jest-base'),
   testMatch: ['<rootDir>/packages/*/{src,test}/**/*.test.{ts,tsx}'],
   projects: ['<rootDir>/packages/*'],
+  collectCoverage: true,
+  coverageDirectory: '<rootDir>/coverage',
+  coverageReporters: ['json-summary', 'text', 'text-summary'],
+  collectCoverageFrom: ['src/**/*.ts'],
 };
--- config/jest.js
+++ config/jest.js
@@ -1,9 +1,20 @@
 module.exports = {
   ...require('./jest-base'),
   testMatch: ['<rootDir>/{src,test}/**/*.test.{ts,tsx}'],
+  // process.env.JEST_IS_MONOREPO is set to 'true' when project loads
+  // @jupiterone/typescript-tools/config/jest-monorepo file for configuring
+  // monorepo to use `jest`.
   ...(process.env.JEST_IS_MONOREPO === 'true'
-    ? {}
-    : {
+    ? // If this config is for a package/project within a monorepo then we
+      // don't include the coverage configuration properties because they
+      // will produce jest config validation warnings.
+      // The coverage-related properties should exist at the root jest.config.js
+      // file for a monorepo (not within the packages/*/jest.config.js files).
+      {}
+    : // If this is not a monorepo, then include the coverage-related
+      // configuration properties.
+      {
+        coverageDirectory: '<rootDir>/coverage',
         collectCoverage: true,
         coverageReporters: ['json-summary', 'text', 'text-summary'],
         collectCoverageFrom: [
